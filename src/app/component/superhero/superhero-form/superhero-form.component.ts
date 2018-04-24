b4607f910e208d1eebe04a2bf604df2241cfe143
INTECH\adriano.franci <adriano.Franci@gmail.com>
2018-04-24 09:14:55 +0200

    added form field

--- src/app/component/superhero/superhero-form/superhero-form.component.ts
+++ src/app/component/superhero/superhero-form/superhero-form.component.ts
@@ -13,6 +13,15 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class SuperheroFormComponent {
   heroInformation: any = {};
   heroForm: FormGroup;
+  heroFormFields = [
+    { name: 'heroName', label: 'Hero name', type: 'text', placeholder: 'Iron Man' },
+    { name: 'intelligence', label: 'Intelligence', type: 'number' },
+    { name: 'strength', label: 'Strength', type: 'number'},
+    { name: 'speed', label: 'Speed', type: 'number'},
+    { name: 'durability', label: 'Durability', type: 'number'},
+    { name: 'power', label: 'Power', type: 'number'},
+    { name: 'combat', label: 'Combat', type: 'number'},
+  ];
 
   constructor(public dialogRef: MatDialogRef<SuperheroFormComponent>,
               private _heroService: HeroService,
@@ -35,13 +44,5 @@ export class SuperheroFormComponent {
     this.dialogRef.close();
   }
 
-  // heroFormFields = [
-  //   { name: 'heroName', label: 'Hero name', type: 'text', placeholder: 'Iron Man' },
-  //   { name: 'intelligence', label: 'Intelligence', type: 'number' },
-  //   { name: 'strength', label: 'Strength', type: 'number'},
-  //   { name: 'speed', label: 'Speed', type: 'number'},
-  //   { name: 'durability', label: 'Durability', type: 'number'},
-  //   { name: 'power', label: 'Power', type: 'number'},
-  //   { name: 'combat', label: 'Combat', type: 'number'},
-  // ];
+
 }
